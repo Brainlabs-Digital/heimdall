927a5a9799a24218a8f009883effa0ff17187e1c
Tom Anthony <git@tomanthony.co.uk>
2015-09-30 11:01:23 +0100

    Version -> 0.0.4. Fix manifest again.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='heimdall',
     packages=['heimdall'],
-    version='0.0.3',
+    version='0.0.4',
     description='A library for taking website screenshots, whilst emulating various devices and resolutions.',
     author='Distilled R&D',
     author_email='tom.anthony@distilled.net',
     url='https://github.com/DistilledLtd/heimdall',
-    download_url='https://github.com/DistilledLtd/heimdall/tarball/0.0.3',
+    download_url='https://github.com/DistilledLtd/heimdall/tarball/0.0.4',
     keywords=['screenshot', 'phantomjs'],
     classifiers=[],
     install_requires=[
