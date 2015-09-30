dd2c8d371cbe33fa1a2b0152b8f139d61b672b9d
Tom Anthony <git@tomanthony.co.uk>
2015-09-30 10:30:35 +0100

    Bump version number -> 0.0.2.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+    name='heimdall',
+    packages=['heimdall'],
+    version='0.0.2',
+    description='A library for taking website screenshots, whilst emulating various devices and resolutions.',
+    author='Distilled R&D',
+    author_email='tom.anthony@distilled.net',
+    url='https://github.com/DistilledLtd/heimdall',
+    download_url='https://github.com/DistilledLtd/heimdall/tarball/0.0.2',
+    keywords=['screenshot', 'phantomjs'],
+    classifiers=[],
+    install_requires=[
+        'Pillow',
+        'wheel'
+    ]
+)
