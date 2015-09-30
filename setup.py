07f8b277a00d298d4695f7719c1123ee178906d6
Tom Anthony <git@tomanthony.co.uk>
2015-09-30 11:26:43 +0100

    Version -> 0.0.6. Add JS files to setup.py - having trouble making them install.

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
+
 setup(
     name='heimdall',
     packages=['heimdall'],
-    version='0.0.5',
+    version='0.0.6',
     description='A library for taking website screenshots, whilst emulating various devices and resolutions.',
     author='Distilled R&D',
     author_email='tom.anthony@distilled.net',
     url='https://github.com/DistilledLtd/heimdall',
-    download_url='https://github.com/DistilledLtd/heimdall/tarball/0.0.5',
+    download_url='https://github.com/DistilledLtd/heimdall/tarball/0.0.6',
+    package_data={'': ['take_screenshot.js', 'optparse.js']},
     keywords=['screenshot', 'phantomjs'],
     classifiers=[],
     install_requires=[
